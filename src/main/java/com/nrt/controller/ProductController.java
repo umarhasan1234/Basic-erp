fc8df702170ce46feec5b028bca1df2260be5bd8
Umarhasan733 <umarhasan733@gmail.com>
2023-08-21 18:53:32 +0530

    implements find product by id

--- src/main/java/com/nrt/controller/ProductController.java
+++ src/main/java/com/nrt/controller/ProductController.java
@@ -1,23 +1,21 @@
 package com.nrt.controller;
 
-import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.nrt.entity.Product;
-import com.nrt.repository.ProductRepository;
 import com.nrt.request.ProductRequest;
 import com.nrt.service.ProductService;
+
 
 @Controller
 public class ProductController {
@@ -33,6 +31,11 @@ public class ProductController {
 		modelAndView.setViewName("/html/product/add_Product");
 		return modelAndView;
 	}
+	
+	 @GetMapping("/favicon.ico")
+	    public ResponseEntity<Void> favicon() {
+	        return ResponseEntity.noContent().build();
+	    }
 
 	// this method Add product
 	 @RequestMapping("/saveProduct")
